b8b3b4c16f7f2e3fb592081942764a5b6d5295c5
OMCH <omch20012@gmail.com>
2020-12-29 12:29:57 -0500

    Rehaul of site, new CSS, started nav bar autogen

---
+++ scripts/navLoader.js
@@ -0,0 +1,53 @@
+var menuOptions = [
+  "home",
+  "current",
+  "photography",
+  "graphics",
+  "drawings",
+  "jewelery",
+  "the tacky bee",
+  "blog"
+];
+
+loadMenu();
+
+function loadMenu() {
+  if (document.getElementById("nav") == null) {
+    var boxes = document.getElementById("boxes");
+
+    for (var i = 0; i < menuOptions.length; i++) {
+      if (i >= 1) {
+        var fig = document.createElement("FIGURE");
+        boxes.appendChild(fig);
+        var title = document.createElement("H2");
+        title.innerHTML = menuOptions[i];
+        fig.appendChild(title);
+        var box = document.createElement("IMG");
+        box.src = "images/" + menuOptions[i] + ".jpg";
+        box.alt = "[" + menuOptions[i] + "]";
+        fig.appendChild(box);
+
+        if (i == 1) {
+          fig.setAttribute("id", "mainImage");
+        }
+      }
+    }
+  }
+  else {
+    var nav = document.getElementById("nav");
+
+    for (var i = 0; i < menuOptions.length; i++) {
+      var anchor = document.createElement("A");
+      nav.appendChild(anchor);
+
+      anchor.innerHTML = menuOptions[i];
+
+      if (i == 0) {
+        anchor.setAttribute("href", "\\");
+      }
+      else {
+        anchor.setAttribute("href", "\\pages/" + menuOptions[i] + ".html");
+      }
+    }
+  }
+}
